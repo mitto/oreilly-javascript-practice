80a4f3604667494345c8e7d0b2949b984e78d81d
mitto nagisa <mitto@mittostar.info>
2020-04-28 04:10:04 +0900

    import all files in 2011/4/11 ~ 2011/8/9

---
+++ 初めてのJavaScript/9-8 service.php
@@ -0,0 +1,25 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Style-Type" content="text/css" />
+		<meta http-equiv="Content-Script-Type" content="text/javascript" />
+		<title>9-8 例9-7で発生するリクエストを処理するPHPアプリケーション</title>
+	</head>
+	<body>
+		<?php
+			
+			$color = $_GET['color']; //"?color="に続く文字列が$colorに代入される
+			if ($color == 'red') {
+				print("<div id='val1'>バラ</div>");
+				print("<div id='val2'>リンゴ</div>");
+			}
+			else if ($color == "blue") {
+				print("<div id='val1'>ブルーベリー</div>");
+				print("<div id='val2'>空</div>");
+			}
+			
+		?>
+	</body>
+</html>
---
+++ 初めてのJavaScript/ch14-02.php
@@ -0,0 +1,39 @@
+﻿<?php
+	if (empty($_GET['pref'])) {
+		echo "都道府県が指定されていません";
+	}
+	else {
+		//検索対象から始めと終わりのホワイトスペースを取り除く
+		$search = trim($_GET['pref']);
+		switch ($search) {
+			case "HO" :
+				$result = "<option value='札幌'>札幌</option>" .
+				"<option value='旭川'>旭川</option>" .
+				"<option value='根室'>根室</option>";
+				break;
+			
+			case "FU" :
+				$result = "<option value='福井'>福井</option>" .
+				"<option value='鯖江'>鯖江</option>" .
+				"<option value='越前'>越前</option>";
+				break;
+				
+			case "NA" :
+				$result = "<option value='長野'>長野</option>" .
+				"<option value='松本'>松本</option>" .
+				"<option value='Web 3.0シティ'>Web 3.0シティ</option>";
+				break;
+				
+			case "NA" :
+				$result = "<option value='那覇'>那覇</option>" .
+				"<option value='石垣'>石垣</option>" .
+				break;
+				
+			default :
+				$result = "都市が見つかりません";
+				break;
+
+		}
+		echo $result;
+	}
+?>
